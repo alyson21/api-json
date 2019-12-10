aa78a2e3563c3e80df0fe92298396a76b14067b0
alyson <71701117@aluno.faculdadecotemig.br>
2019-12-10 10:00:44 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class SeriesController
+{
+    public function index(){
+        return  [
+            "Grey's Anatomy",
+            "Lost"
+        ];
+    }
+}
